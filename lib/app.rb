3822ff909f10ca790008f6004c9d707591629189
Minh Chau <nmchau87@gmail.com>
2019-05-01 20:33:05 +0900

    dev: fix encoding bug

--- lib/app.rb
+++ lib/app.rb
@@ -4,7 +4,7 @@ require 'github/markup'
 class App < Roda
   route do |r|
     r.post String do |format|
-      GitHub::Markup.render('markup.' + format, request.body.read)
+      GitHub::Markup.render('markup.' + format, request.body.read.force_encoding('utf-8'))
     end
   end
 end
